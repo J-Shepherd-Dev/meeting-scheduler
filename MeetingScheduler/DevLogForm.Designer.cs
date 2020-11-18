3c680a222c6a7d43e1453a3365b7b5965c006f83
Jack Carey <jack@jackcarey.co.uk>
2020-11-18 00:27:55 +0000

    updated log font to consolas

--- MeetingScheduler/DevLogForm.Designer.cs
+++ MeetingScheduler/DevLogForm.Designer.cs
@@ -36,6 +36,7 @@
             this.devLogFlowPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.devLogFlowPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.devLogFlowPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.devLogFlowPanel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.devLogFlowPanel.Location = new System.Drawing.Point(0, 0);
             this.devLogFlowPanel.Name = "devLogFlowPanel";
             this.devLogFlowPanel.Size = new System.Drawing.Size(540, 478);
