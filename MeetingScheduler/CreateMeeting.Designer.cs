eb3fbff0f99e37efd56a8660aceb59593b548306
Jack Carey <jack@jackcarey.co.uk>
2020-11-20 12:10:42 +0000

    added labels to meeting invitee table

--- MeetingScheduler/CreateMeeting.Designer.cs
+++ MeetingScheduler/CreateMeeting.Designer.cs
@@ -36,13 +36,17 @@
             this.newMeetingInvitees = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.newMeetingSlotsGroupBox = new System.Windows.Forms.GroupBox();
-            this.newMeetingBtnPanel = new System.Windows.Forms.Panel();
-            this.newMeetingSaveBtn = new System.Windows.Forms.Button();
-            this.newMeetingCancelBtn = new System.Windows.Forms.Button();
             this.calendarPanel1 = new MeetingScheduler.CalendarPanel();
+            this.newMeetingBtnPanel = new System.Windows.Forms.Panel();
+            this.newMeetingCancelBtn = new System.Windows.Forms.Button();
+            this.newMeetingSaveBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.newMeetingInvitees.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.newMeetingSlotsGroupBox.SuspendLayout();
             this.newMeetingBtnPanel.SuspendLayout();
             this.SuspendLayout();
@@ -107,18 +111,24 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 168F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.39735F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.60265F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 131F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 22);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 5;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.52632F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.47369F));
+            this.tableLayoutPanel1.RowCount = 8;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.Size = new System.Drawing.Size(283, 365);
             this.tableLayoutPanel1.TabIndex = 0;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
@@ -136,6 +146,15 @@
             this.newMeetingSlotsGroupBox.TabStop = false;
             this.newMeetingSlotsGroupBox.Text = "Meeting Slots";
             // 
+            // calendarPanel1
+            // 
+            this.calendarPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.calendarPanel1.Location = new System.Drawing.Point(3, 22);
+            this.calendarPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.calendarPanel1.Name = "calendarPanel1";
+            this.calendarPanel1.Size = new System.Drawing.Size(404, 365);
+            this.calendarPanel1.TabIndex = 0;
+            // 
             // newMeetingBtnPanel
             // 
             this.newMeetingBtnPanel.Controls.Add(this.newMeetingCancelBtn);
@@ -146,17 +165,6 @@
             this.newMeetingBtnPanel.Padding = new System.Windows.Forms.Padding(3);
             this.newMeetingBtnPanel.Size = new System.Drawing.Size(699, 35);
             this.newMeetingBtnPanel.TabIndex = 4;
-            // 
-            // newMeetingSaveBtn
-            // 
-            this.newMeetingSaveBtn.Dock = System.Windows.Forms.DockStyle.Right;
-            this.newMeetingSaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.newMeetingSaveBtn.Location = new System.Drawing.Point(621, 3);
-            this.newMeetingSaveBtn.Name = "newMeetingSaveBtn";
-            this.newMeetingSaveBtn.Size = new System.Drawing.Size(75, 29);
-            this.newMeetingSaveBtn.TabIndex = 0;
-            this.newMeetingSaveBtn.Text = "Create";
-            this.newMeetingSaveBtn.UseVisualStyleBackColor = true;
             // 
             // newMeetingCancelBtn
             // 
@@ -170,14 +178,54 @@
             this.newMeetingCancelBtn.UseVisualStyleBackColor = true;
             this.newMeetingCancelBtn.Click += new System.EventHandler(this.newMeetingCancelBtn_Click);
             // 
-            // calendarPanel1
+            // newMeetingSaveBtn
             // 
-            this.calendarPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.calendarPanel1.Location = new System.Drawing.Point(3, 22);
-            this.calendarPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.calendarPanel1.Name = "calendarPanel1";
-            this.calendarPanel1.Size = new System.Drawing.Size(404, 365);
-            this.calendarPanel1.TabIndex = 0;
+            this.newMeetingSaveBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.newMeetingSaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newMeetingSaveBtn.Location = new System.Drawing.Point(621, 3);
+            this.newMeetingSaveBtn.Name = "newMeetingSaveBtn";
+            this.newMeetingSaveBtn.Size = new System.Drawing.Size(75, 29);
+            this.newMeetingSaveBtn.TabIndex = 0;
+            this.newMeetingSaveBtn.Text = "Create";
+            this.newMeetingSaveBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Image";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(154, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Important?";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // CreateMeeting
             // 
@@ -198,6 +246,8 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.newMeetingInvitees.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.newMeetingSlotsGroupBox.ResumeLayout(false);
             this.newMeetingBtnPanel.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -216,5 +266,8 @@
         private System.Windows.Forms.Button newMeetingCancelBtn;
         private System.Windows.Forms.Button newMeetingSaveBtn;
         private CalendarPanel calendarPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
