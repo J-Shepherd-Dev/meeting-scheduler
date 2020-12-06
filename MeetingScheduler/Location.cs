d35477e1211eb401ce36019d70c35b945db5281b
Jack Carey <jack@jackcarey.co.uk>
2020-12-06 16:02:34 +0000

    renamed dance party - no longer national due to covid-19 restrictions

--- MeetingScheduler/Location.cs
+++ MeetingScheduler/Location.cs
@@ -9,10 +9,31 @@ namespace MeetingScheduler
     public class Location
     {
         public string Name = "Location";
-        public bool isWheelChairAccessible = false;
+        public int capacity = 1;
+        public List<Equipment> fixedEquipment = new List<Equipment>();
+        public bool isWheelChairAccessible {
+            get {
+                return fixedEquipment.Contains(AllEquipment.WheelchairAccess);
+            }
+        }
         public Location(string name)
         {
             this.Name = name;
+        }
+        public Location(string name, int capacity) {
+            this.Name = name;
+            if (capacity >= 1)
+            {
+                this.capacity = capacity;
+            }
+        }
+        public Location(string name, int capacity,List<Equipment> fixedEquipment)
+        {
+            this.Name = name;
+            if (capacity >= 1)
+            {
+                this.capacity = capacity;
+            }
         }
 
         public override string ToString()
