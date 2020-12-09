f7ce44931e0570d2bc2f9f56f4067ff7fe7ba44b
b9038830 <b9038830@my.shu.ac.uk>
2020-12-09 22:02:32 +0900

    Move user story meetings to AllMeetings.Reset

--- MeetingScheduler/AllMeetings.cs
+++ MeetingScheduler/AllMeetings.cs
@@ -35,5 +35,126 @@ namespace MeetingScheduler
             //Send event
             meetingsUpdated?.Invoke(null, new EventArgs());
         }
+
+        public static void Reset()
+        {
+            // Reset meetings
+            lastMeeting = null;
+            meetings.Clear();
+
+            // Create Mehmet's funky dance party
+            TimeSpan untilBeginning = new TimeSpan((int)DateTime.Today.DayOfWeek, 0, 0, 0);
+
+            DateTime currentWeek = (DateTime.Today - untilBeginning).Date;
+
+            /*
+             * Add the meetings we need for user stories
+             */
+
+            //DANCE PARTY
+            Meeting funkyDanceParty = new Meeting(
+                AllUsers.tom,
+                "Mehmet's Funky Dance Party",
+                currentWeek + new TimeSpan(
+                    2,  // Tuesday
+                    12, 0, 0  // At 12 pm
+                ),
+                2
+            );
+            //add a description
+            funkyDanceParty.Details = "Mehmet's holding his annual funky dance party! Don your best 60's outfit then get ready to dance and feel a rhythm through a mixture of soul, jazz, and R&B!";
+            // Everyone is invited to Mehmet's funky dance party
+            funkyDanceParty.Participants.Add(new Participant(AllUsers.jack));
+            funkyDanceParty.Participants.Add(new Participant(AllUsers.john));
+            //Tom, Ryan, and Mehmet are all attending with varying statuses 
+            funkyDanceParty.Participants.Add(new Participant(AllUsers.mehmet, 2, true));
+            funkyDanceParty.Participants.Add(new Participant(AllUsers.ryan, 0, true));
+            funkyDanceParty.Participants.Add(new Participant(AllUsers.tom, 1, true));
+
+            //USER STORY 1
+            Meeting UserStory1AccCrit1 = new Meeting(
+                AllUsers.jack,
+                "Meeting for Ryan",
+                currentWeek + new TimeSpan(1, 11, 0, 0),
+                1
+                );
+            UserStory1AccCrit1.Details = "Jack and Ryan chit-chat.";
+            Participant us1ac1Jack = new Participant(AllUsers.jack);
+            Participant us1ac1Ryan = new Participant(AllUsers.ryan);
+            UserStory1AccCrit1.Participants.Add(us1ac1Jack);
+            UserStory1AccCrit1.Participants.Add(us1ac1Ryan);
+
+            Meeting UserStory1AccCrit2 = new Meeting(
+            AllUsers.tom,
+            "User Story 1 - Acceptance Criteria 2",
+            currentWeek + new TimeSpan(1, 12, 00, 0),
+            1);
+            UserStory1AccCrit2.Details = "User Story 1 - Acceptance Criteria 2";
+            Participant us1ac2Jack = new Participant(AllUsers.jack);
+            Participant us1ac2Ryan = new Participant(AllUsers.ryan);
+            UserStory1AccCrit2.Participants.Add(us1ac2Jack);
+            UserStory1AccCrit2.Participants.Add(us1ac2Ryan);
+
+            //USER STORY 2
+
+            //USER STORY 3
+            Meeting LaptoprackInspection = new Meeting(
+                AllUsers.john,
+                "Laptop Rack Inspection",
+                currentWeek + new TimeSpan(2, 9, 0, 0),
+                9);
+            Participant laptoprackJohn = new Participant(AllUsers.john, 1, true);
+            laptoprackJohn.equipmentRequests.Add(AllEquipment.LaptopRack);
+            LaptoprackInspection.Details = "John will be checking the laptop rack for inspections";
+            LaptoprackInspection.Participants.Add(laptoprackJohn);
+
+            //User STORY 4
+
+            //USER STORY 5
+            Meeting NetworkLabMeeting = new Meeting(
+                AllUsers.tom,
+                "Network Lab Demonstration",
+                currentWeek + new TimeSpan(3, 9, 0, 0),
+                9);
+            Participant networkLabTom = new Participant(AllUsers.tom, 1, true);
+            networkLabTom.locationPreferences.Add(AllLocations.NetworkLab);
+            NetworkLabMeeting.Details = "Tom and Ryan use the network lab";
+            NetworkLabMeeting.Participants.Add(networkLabTom);
+            NetworkLabMeeting.Participants.Add(new Participant(AllUsers.ryan, 1, true));
+
+            Meeting JohnsNetworkLabMeeting = new Meeting(AllUsers.john,
+                "Johns Network Lab #1",
+                currentWeek + new TimeSpan(3, 10, 0, 0),
+                1);
+            Participant networkJohn = new Participant(AllUsers.john, 1, true);
+            networkJohn.locationPreferences.Add(AllLocations.NetworkLab);
+            JohnsNetworkLabMeeting.Participants.Add(networkJohn);
+            JohnsNetworkLabMeeting.Participants.Add(new Participant(AllUsers.jack, 0, true));
+            JohnsNetworkLabMeeting.Participants.Add(new Participant(AllUsers.mehmet, 0, true));
+
+            Meeting JohnsNetworkLabMeeting2 = new Meeting(AllUsers.john,
+                "Johns Network Lab #2",
+                currentWeek + new TimeSpan(4, 10, 0, 0),
+                1);
+            Participant networkJohn2 = new Participant(AllUsers.john, 1, true);
+            networkJohn.locationPreferences.Add(AllLocations.NetworkLab);
+            JohnsNetworkLabMeeting.Participants.Add(networkJohn2);
+            JohnsNetworkLabMeeting.Participants.Add(new Participant(AllUsers.jack, 0, true));
+            JohnsNetworkLabMeeting.Participants.Add(new Participant(AllUsers.mehmet, 0, true));
+
+            /*
+             * Save the meetings in the static class
+             */
+            meetings.Add(funkyDanceParty);
+            meetings.Add(UserStory1AccCrit1);
+            meetings.Add(UserStory1AccCrit2);
+            meetings.Add(NetworkLabMeeting);
+            meetings.Add(JohnsNetworkLabMeeting);
+            meetings.Add(JohnsNetworkLabMeeting2);
+            meetings.Add(LaptoprackInspection);
+            
+            //Send event
+            meetingsUpdated?.Invoke(null, new EventArgs());
+        }
     }
 }
